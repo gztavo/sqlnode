232434b2d3b2b759be4a6b7a8a917191520633f7
Gustavo Costa Rosa <gcrosa@latam.stefanini.com>
2020-03-31 00:19:37 -0300

    Configuração do SQL no DOCKER

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports ={
+    dialect:'mssql',
+    host: 'localhost',
+    username:'sa',
+    password:'sql2019Teste',
+    database:'TesteContainer',
+    port:11433,
+    define:{
+        timesstamps: true,
+        underscored: true,
+    },
+};
